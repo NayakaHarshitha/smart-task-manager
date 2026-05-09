424b72cd9092a9201d4dad966136ae16c31fa27a
dixith82 <dixithreddy82@gmail.com>
2026-05-09 18:54:04 +0530

    Deployment ready

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
 
     SECRET_KEY = 'secretkey123'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@localhost:5432/taskmanager'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
